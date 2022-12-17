ec6f05d3a1fd92bc9c34edd5e4462ccedd381d12
tiltozavrik <106474343+tiltozavrik@users.noreply.github.com>
2022-12-17 18:01:56 +0700

    create structure

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,34 @@
-const Header = () => {
+import styles from './header.module.css'
 
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <nav className={styles.menu}>
+                <ul className={styles.menu__items}>
+                    <li className={styles.menu__item}>
+                        <a href="/" className={styles.menu__link}>
+
+                        </a>
+                    </li>
+                    <li className={styles.menu__item}>
+                        <a href="/" className={styles.menu__link}>
+
+                        </a>
+                    </li>
+                    <li className={styles.menu__item}>
+                        <a href="/" className={styles.menu__link}>
+                            <Logo/>
+                        </a>
+                    </li>
+                    <li className={styles.menu__item}>
+                        <a href="/" className={styles.menu__link}>
+
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default Header;
