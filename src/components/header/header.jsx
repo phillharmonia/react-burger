c994bb4713a7fc2140c46750408cc3efc8624ee4
tiltozavrik <106474343+tiltozavrik@users.noreply.github.com>
2022-12-19 16:26:41 +0700

    create header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,24 @@
 import styles from './header.module.css'
+import {
+    Logo,
+    BurgerIcon,
+    ListIcon,
+    ProfileIcon
+} from '@ya.praktikum/react-developer-burger-ui-components'
 
 const Header = () => {
     return (
         <div className={styles.header}>
             <nav className={styles.menu}>
-                <ul className={styles.menu__items}>
-                    <li className={styles.menu__item}>
+                <ul className={`${styles.menu__items}`}>
+                    <li className={`${styles.menu__item} ${styles.menu__item_left}`}>
                         <a href="/" className={styles.menu__link}>
-
+                            <BurgerIcon type="primary"/>
+                            <p className={styles.menu__text}>Конструктор</p>
                         </a>
-                    </li>
-                    <li className={styles.menu__item}>
                         <a href="/" className={styles.menu__link}>
-
+                            <ListIcon type="secondary"></ListIcon>
+                            <p className={styles.menu__text}>Лента заказов</p>
                         </a>
                     </li>
                     <li className={styles.menu__item}>
@@ -22,7 +28,8 @@ const Header = () => {
                     </li>
                     <li className={styles.menu__item}>
                         <a href="/" className={styles.menu__link}>
-
+                            <ProfileIcon/>
+                            <p className={styles.menu__text}>Личный кабинет</p>
                         </a>
                     </li>
                 </ul>
