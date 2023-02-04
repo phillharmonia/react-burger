e55652ffbae8eb043761c4a62c3613c17c1a9d56
tiltozavrik <106474343+phillharmonia@users.noreply.github.com>
2023-02-04 15:14:34 +0700

    create ingridient details

--- src/components/BurgerIngridiens/BurgerIngridiens.jsx
+++ src/components/BurgerIngridiens/BurgerIngridiens.jsx
@@ -8,27 +8,27 @@ import IngridientDetails from "../IngridientDetails/IngridientDetails";
 import {useDispatch, useSelector} from "react-redux";
 import {getIngridients} from "../../services/actions/Ingridients";
 import {useInView} from "react-intersection-observer";
+import {closeIngridientDetails, getIngridientDetails} from "../../services/actions/IngridientsDetails";
 
 const BurgerIngridiens = () => {
     const [current, setCurrent] = React.useState('bun')
     const [popupActive, setPopupActive] = useState(false)
-    const [details, setDetails] = useState({});
     const dispatch = useDispatch()
     const ingridients = useSelector(store => store.ingridients.ingridients)
     useEffect(() => {
         dispatch(
             getIngridients())
-    },[dispatch])
+    }, [dispatch])
     const openPopup = () => {
         setPopupActive(true)
     }
     const getDetails = (props) => {
-        setDetails(props)
+        dispatch(getIngridientDetails(props))
     }
-    const { ref: bunRef, inView: bunInView } = useInView({
+    const {ref: bunRef, inView: bunInView} = useInView({
         threshold: 0.5
     })
-    const { ref: sauceRef, inView: sauceInView } = useInView({
+    const {ref: sauceRef, inView: sauceInView} = useInView({
         threshold: 0.5
     })
     const {ref: mainRef, inView: mainInView} = useInView({
@@ -56,7 +56,7 @@ const BurgerIngridiens = () => {
     }
     useEffect(() => {
         positionScroll()
-    },[bunInView, sauceInView, mainInView])
+    }, [bunInView, sauceInView, mainInView])
     return (
         <section className={`${styles.section} mt-10`}>
             <h1 className={`mb-5 text text_type_main-large`}>Соберите бургер</h1>
@@ -72,58 +72,78 @@ const BurgerIngridiens = () => {
                 </Tab>
             </nav>
             <div className={`${styles.ingridient_list}`}>
-            <div className={`${styles.ingridients} mb-10`}>
-                <h3 ref={bunRef} id='bun' className={`text text_type_main-medium mb-6`}>Булки</h3>
-                <ul className={`${styles.ingridient_items}`}>
-                    <> { ingridients.map(props =>
+                <div className={`${styles.ingridients} mb-10`}>
+                    <h3 ref={bunRef} id='bun' className={`text text_type_main-medium mb-6`}>Булки</h3>
+                    <ul className={`${styles.ingridient_items}`}>
+                        <> {ingridients.map(props =>
                             props.type === 'bun' &&
-                                <li className={styles.ingridient_item} key={props._id} onClick={()=> {openPopup();getDetails(props)}}>
-                                    <img className={styles.image} src={props.image} alt={props.name}/>
-                                    <div className={`${styles.price} mt-1 mb-1`}>
-                                        <p className={`text text_type_digits-default`}>{props.price}</p>
-                                        <CurrencyIcon type='primary' />
-                                    </div>
-                                    <p className={`text text_type_main-default`}>{props.name}</p>
-                                    <Counter count={1} size="default" />
-                                </li>)
+                            <li className={styles.ingridient_item} key={props._id} onClick={() => {
+                                openPopup();
+                                getDetails(props)
+                            }}>
+                                <img className={styles.image} src={props.image} alt={props.name}/>
+                                <div className={`${styles.price} mt-1 mb-1`}>
+                                    <p className={`text text_type_digits-default`}>{props.price}</p>
+                                    <CurrencyIcon type='primary'/>
+                                </div>
+                                <p className={`text text_type_main-default`}>{props.name}</p>
+                                <Counter count={1} size="default"/>
+                            </li>)
                         }
                         </>
-                </ul>
-            </div>
-            <div className={`${styles.ingridients} mt-10 mb-10`}>
-                <h3 ref={sauceRef} id='sauce' className={`text text_type_main-medium mb-6`}>Соусы</h3>
-                <ul className={`${styles.ingridient_items}`}>
-                    { ingridients.map(props =>
-                        props.type === 'sauce' &&
-                        <li className={styles.ingridient_item} key={props._id} onClick={()=> {openPopup();getDetails(props)}}>
-                            <img className={styles.image} src={props.image} alt={props.name}/>
-                            <div className={`${styles.price} mt-1 mb-1`}>
-                                <p className={`text text_type_digits-default`}>{props.price}</p>
-                                <CurrencyIcon type='primary' />
-                            </div>
-                            <p className={`text text_type_main-default`}>{props.name}</p>
-                            <Counter count={1} size="default" />
-                        </li>)
-                    }
-                </ul>
-            </div>
-            <div className={`${styles.ingridients} mt-10 mb-10`}>
-                <h3 ref={mainRef} id='main' className={`text text_type_main-medium mb-6`}>Начинки</h3>
-                <ul className={`${styles.ingridient_items}`}>
-                    { ingridients.map(props =>
-                        props.type === 'main' &&
-                        <li className={styles.ingridient_item} key={props._id} onClick={()=> {openPopup();getDetails(props)}}>
-                            <img className={styles.image} src={props.image} alt={props.name}/>
-                            <div className={`${styles.price} mt-1 mb-1`}>
-                                <p className={`text text_type_digits-default`}>{props.price}</p>
-                                <CurrencyIcon type='primary' />
-                            </div>
-                            <p className={`text text_type_main-default`}>{props.name}</p>
-                            <Counter count={1} size="default" />
-                        </li>)
-                    }
-                </ul>
-            </div>
+                    </ul>
+                </div>
+                <div className={`${styles.ingridients} mt-10 mb-10`}>
+                    <h3 ref={sauceRef} id='sauce' className={`text text_type_main-medium mb-6`}>Соусы</h3>
+                    <ul className={`${styles.ingridient_items}`}>
+                        {ingridients.map(props =>
+                            props.type === 'sauce' &&
+                            <li className={styles.ingridient_item} key={props._id} onClick={() => {
+                                openPopup();
+                                getDetails(props)
+                            }}>
+                                <img className={styles.image} src={props.image} alt={props.name}/>
+                                <div className={`${styles.price} mt-1 mb-1`}>
+                                    <p className={`text text_type_digits-default`}>{props.price}</p>
+                                    <CurrencyIcon type='primary'/>
+                                </div>
+                                <p className={`text text_type_main-default`}>{props.name}</p>
+                                <Counter count={1} size="default"/>
+                            </li>)
+                        }
+                    </ul>
+                </div>
+                <div className={`${styles.ingridients} mt-10 mb-10`}>
+                    <h3 ref={mainRef} id='main' className={`text text_type_main-medium mb-6`}>Начинки</h3>
+                    <ul className={`${styles.ingridient_items}`}>
+                        {ingridients.map(props =>
+                            props.type === 'main' &&
+                            <li className={styles.ingridient_item} key={props._id} onClick={() => {
+                                openPopup();
+                                getDetails(props)
+                            }}>
+                                <img className={styles.image} src={props.image} alt={props.name}/>
+                                <div className={`${styles.price} mt-1 mb-1`}>
+                                    <p className={`text text_type_digits-default`}>{props.price}</p>
+                                    <CurrencyIcon type='primary'/>
+                                </div>
+                                <p className={`text text_type_main-default`}>{props.name}</p>
+                                <Counter count={1} size="default"/>
+                            </li>)
+                        }
+                    </ul>
+                </div>
+                {
+                    popupActive && (
+                        <Modal closePopup={() => {
+                            setPopupActive(false)
+                            dispatch(closeIngridientDetails())
+                        }
+                        }>
+                            <IngridientDetails />
+                        </Modal>
+                    )
+                }
             </div>
         </section>
     )
--- src/services/store/store.jsx
+++ src/services/store/store.jsx
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import {ingridientsReducer} from "../reducers/Ingridients";
+import {ingridientsDetailsReducer} from "../reducers/IngridientsDetails"
 
 export const rootReducer = combineReducers({
-    ingridients: ingridientsReducer
+    ingridients: ingridientsReducer,
+    ingridientDetails: ingridientsDetailsReducer
 })
